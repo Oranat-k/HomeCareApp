a32d31d8212da7fe5d91cc642cc6a534a0038a6c
oranat <oranat.kan@gmail.com>
2018-12-12 13:39:12 +0700

    Medicine image upload for gallery

--- app/src/main/java/akkaudom/oranat/th/ac/su/reg/homecarese/AddPlannerActivity.java
+++ app/src/main/java/akkaudom/oranat/th/ac/su/reg/homecarese/AddPlannerActivity.java
@@ -1,6 +1,15 @@
 package akkaudom.oranat.th.ac.su.reg.homecarese;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +17,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class AddPlannerActivity extends AppCompatActivity {
+
+    private CircleImageView ProfileImage;
+    private static final int PICK_IMAGE = 1;
+    Uri imageUri;
+
 
     Button btnBeforMed,btnAfterMed,btnMorning,btnAfternoon,btnEvening,btnBeforeBed;
     EditText nameMedicine,coutMedicine;
@@ -25,10 +46,38 @@ public class AddPlannerActivity extends AppCompatActivity {
 
         // menu bar
         ActionBar actionBar = getSupportActionBar ();
-                actionBar.setTitle ("เพิ่มยา");
+        actionBar.setTitle ("เพิ่มยา");
 
         Createwidget();
 
+        ProfileImage = (CircleImageView) findViewById (R.id.profile_image);
+        ProfileImage.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+
+                Intent gallery = new Intent ();
+                gallery.setType ("image/*");
+                gallery.setAction (Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult (Intent.createChooser (gallery,"Sellect Picture"),PICK_IMAGE);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult (requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE && resultCode == RESULT_OK){
+            imageUri = data.getData ();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap (getContentResolver (),imageUri);
+                ProfileImage.setImageBitmap (bitmap);
+            }catch (IOException e){
+                e.printStackTrace ();
+            }
+        }
     }
 
     public void InsertData(View view) {
@@ -125,4 +174,9 @@ public class AddPlannerActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+
 }
+

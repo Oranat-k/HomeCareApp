d8025bc98b58e7a51d04c9c2ccbaa462c6ca125a
oranat <oranat.kan@gmail.com>
2019-01-31 19:43:34 +0700

    Design home

--- app/src/main/java/akkaudom/oranat/th/ac/su/reg/homecarese/HomeActivity.java
+++ app/src/main/java/akkaudom/oranat/th/ac/su/reg/homecarese/HomeActivity.java
@@ -1,14 +1,29 @@
 package akkaudom.oranat.th.ac.su.reg.homecarese;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ListView;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
 
 import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    FloatingActionMenu materialDesignFAM;
+    FloatingActionButton floatingActionButton1, floatingActionButton2, floatingActionButton3;
+
+    BottomNavigationView mBottomNavigation;
 
 
     @Override
@@ -16,28 +31,81 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_home);
 
-        getSupportActionBar().setTitle("Home");
+//      getSupportActionBar().setTitle("Home");
+
+        getSupportActionBar().hide();
+        //not nev bar
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomBar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) bottomNavigationView.getChildAt(0);
+
+        mBottomNavigation =(BottomNavigationView) findViewById(R.id.bottomBar);
+        mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.itemHome:
+                        Intent Second = new Intent(HomeActivity.this, HomeActivity.class);
+                        startActivity(Second);
+                        break;
+                    case R.id.itemChart:
+                        Intent Third = new Intent(HomeActivity.this, PlannerListActivity.class);
+                        startActivity(Third);
+                        break;
+                    case R.id.itemPlanner:
+                        Intent planner = new Intent(HomeActivity.this, PlannerListActivity.class);
+                        startActivity(planner);
+                        break;
+                    case R.id.itemNoti:
+                        Intent noti = new Intent(HomeActivity.this, PlannerListActivity.class);
+                        startActivity(noti);
+                        break;
+                    case R.id.itemProfile:
+                        Intent profile = new Intent(HomeActivity.this, PlannerListActivity.class);
+                        startActivity(profile);
+                        break;
+                }
+
+                return true;
+            }
+        });
+        for (int i = 0; i < menuView.getChildCount(); i++) {
+            final View iconView = menuView.getChildAt(i).findViewById(android.support.design.R.id.icon);
+            final ViewGroup.LayoutParams layoutParams = iconView.getLayoutParams();
+            final DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            layoutParams.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 28, displayMetrics);
+            layoutParams.width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 28, displayMetrics);
+            iconView.setLayoutParams(layoutParams);
+        }
+
+        materialDesignFAM = (FloatingActionMenu) findViewById(R.id.material_design_android_floating_action_menu);
+        floatingActionButton1 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item1);
+        floatingActionButton2 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item2);
+        floatingActionButton3 = (FloatingActionButton) findViewById(R.id.material_design_floating_action_menu_item3);
+
+        floatingActionButton1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent addPlanner = new Intent(HomeActivity.this,  PlannerActivity.class);
+                startActivity(addPlanner);
+
+            }
+        });
+        floatingActionButton2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu second item clicked
+
+            }
+        });
+        floatingActionButton3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //TODO something when floating action menu third item clicked
+
+            }
+        });
 
     }
 
-         public void onClickButtonMenu(View view){
-        switch (view.getId()) {
-            case R.id.homepageM:
-                break;
-            case R.id.chartM:
-                startActivity(new Intent(HomeActivity.this, PlannerListActivity.class));
-                break;
-            case R.id.plannerM:
-                startActivity(new Intent(HomeActivity.this, PlannerListActivity.class));
-                break;
-            case R.id.notiM:
-                startActivity(new Intent(HomeActivity.this, PlannerListActivity.class));
-                break;
-            case R.id.profileM:
-                startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
-                break;
 
-        }
-    }//next page
 
 }
